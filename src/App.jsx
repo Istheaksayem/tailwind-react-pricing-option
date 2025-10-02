9b350a1107505e9fd35024a38138e5849b98cde6
Istheaksayem <istheakahmed9345@gmail.com>
2025-10-02 08:24:13 +0600

    added navber and etc

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import Daiusy from './component/daiusy.nav/daiusy'
+import NavBar from './component/NavBar/NavBar'
+
+function App() {
+
+
+  return (
+    <>
+
+   <header>
+    <NavBar></NavBar>
+       <Daiusy></Daiusy>
+
+   </header>
+    
+
+    </>
+  )
+}
+
+export default App
