7c831ecb3ef806706eb8f1b8b874d063d77ef4ee
Istheaksayem <istheakahmed9345@gmail.com>
2025-10-02 19:50:40 +0600

    added card in daisy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 
+import { Suspense } from 'react'
 import './App.css'
 import Daiusy from './component/daiusy.nav/daiusy'
 import NavBar from './component/NavBar/NavBar'
+import PricingOption from './component/PricingOption/PricingOption'
+
+const pricingPromise = fetch('PricingData.json').then(res => res.json())
+
 
 function App() {
 
@@ -9,12 +14,16 @@ function App() {
   return (
     <>
 
-   <header>
-    <NavBar></NavBar>
-       {/* <Daiusy></Daiusy> */}
+      <header>
+        <NavBar></NavBar>
+        {/* <Daiusy></Daiusy> */}
 
-   </header>
-    
+      </header>
+      <main>
+        <Suspense fallback={<span className="loading loading-spinner text-primary"></span>}>
+          <PricingOption pricingPromise={pricingPromise}></PricingOption>
+        </Suspense>
+      </main>
 
     </>
   )
--- src/component/PricingOption/PricingOption.jsx
+++ src/component/PricingOption/PricingOption.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { use } from 'react';
+import PricingCard from '../PricingCard/PricingCard';
+import DaisyPricingCard from '../DaisyPricingCard/DaisyPricingCard';
 
-const PricingOption = () => {
+const PricingOption = ({pricingPromise}) => {
+
+
+    const pricingData = use(pricingPromise);
+
     return (
         <div>
-            
+            <h2 className='text-5xl mb-4 mt-4'>get Our MemberShip</h2>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                {/* {
+                    pricingData.map(pricing => <PricingCard 
+                        key={pricing.id}
+                        pricing={pricing}></PricingCard>)
+                } */}
+                {
+                    pricingData.map(pricing => <DaisyPricingCard pricing={pricing}></DaisyPricingCard>)
+                }
+            </div>
         </div>
     );
 };
